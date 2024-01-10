ffcc28e3d1c2592a6a223200992dfacd0e72f258
fanuelabebe <fanuelabebe@gmail.com>
2024-01-10 23:02:30 +0300

    chore:updated readme

--- ImageGeneration/open_cv_manuplation.py
+++ ImageGeneration/open_cv_manuplation.py
@@ -8,16 +8,18 @@ img = cv2.imread('/home/fanuel-data/Projects/Week5/AlgorandCertificate/Algorand_
 
 
 
-def put_text_on_image(img,name,company,date,message):
+def put_text_on_image(img,name,company,date,message,message2):
     cv2.putText(img,date,(610,239),cv2.FONT_HERSHEY_PLAIN,1,(0,0,0),2)
     cv2.putText(img,company,(310,300),cv2.FONT_HERSHEY_SIMPLEX,2,(153,0,0),2)
     cv2.putText(img,name,(306,340),cv2.FONT_HERSHEY_PLAIN,2,(0,0,51),3)
-    cv2.putText(img,message,(306,400),cv2.FONT_HERSHEY_PLAIN,1,(0,0,51),2)
+    cv2.putText(img,message,(306,400),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,51),2)
+    cv2.putText(img,message2,(306,420),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,51),2)
 
 
     cv2.imshow('Image', img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-message = "This certificate is given to Trainee to indicate\n that they finished their challenge successfully"
-put_text_on_image(img,"Fanuel Abebe","10 Academy","January 10, 2024",message)
+message1 = "This certificate is given to Trainee to indicate"
+message2 = "that they finished their challenge successfully"
+put_text_on_image(img,"Fanuel Abebe","10 Academy","January 10, 2024",message1,message2)
