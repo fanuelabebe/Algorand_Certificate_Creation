fa59c4f61e2f262f4fbdeac08ac74c66a56da954
fanuelabebe <fanuelabebe@gmail.com>
2024-01-10 22:49:38 +0300

    feat:added dynamic text to image

--- ImageGeneration/open_cv_manuplation.py
+++ ImageGeneration/open_cv_manuplation.py
@@ -1,8 +1,23 @@
 import cv2
 import os
-img = cv2.imread('image/openai_image_test_1.PNG',1)
-img = cv2.resize(img, (0,0),fx = 0.5, fy = 0.5)
+img = cv2.imread('/home/fanuel-data/Projects/Week5/AlgorandCertificate/Algorand_Certificate_Creation/ImageGeneration/asset/openai_image_test_1.PNG',1)
+# img = cv2.resize(img, (0,0),fx = 0.5, fy = 0.5)
 
-cv2.imshow('Image', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# cv2.putText(img,"Fanuel Abebe",(160,129),cv2.FONT_HERSHEY_PLAIN,1,(0,255,0),2)
+
+
+
+
+def put_text_on_image(img,name,company,date,message):
+    cv2.putText(img,date,(610,239),cv2.FONT_HERSHEY_PLAIN,1,(0,0,0),2)
+    cv2.putText(img,company,(310,300),cv2.FONT_HERSHEY_SIMPLEX,2,(153,0,0),2)
+    cv2.putText(img,name,(306,340),cv2.FONT_HERSHEY_PLAIN,2,(0,0,51),3)
+    cv2.putText(img,message,(306,400),cv2.FONT_HERSHEY_PLAIN,1,(0,0,51),2)
+
+
+    cv2.imshow('Image', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+message = "This certificate is given to Trainee to indicate\n that they finished their challenge successfully"
+put_text_on_image(img,"Fanuel Abebe","10 Academy","January 10, 2024",message)
