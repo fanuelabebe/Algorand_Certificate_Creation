bb4ea7eb15f7806fabeb97047ea2ca05b12bc3e3
fanuelabebe <fanuelabebe@gmail.com>
2024-01-13 18:43:52 +0300

    feat:implimented routing and navigation

--- Frontend/algorand-react/src/App.js
+++ Frontend/algorand-react/src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import api from './api/api'
+import 'bootstrap/dist/css/bootstrap.css'
+import {Routes, Route, useNavigate} from 'react-router-dom';
+import AdminPage from './Pages/Admin';
+import HomePage from './Pages/Home';
+import Trainee from './Pages/Trainee'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+
+ 
+
+  const [accounts, setAccounts] = useState([]);
+  const [formData, setFormData] = useState({
+    user_name: '',
+    password: ''
+  });
+
+const fetchAccounts = async () => {
+  const response = await api.get('/accounts/')
+  setAccounts(response.data)
+};
+
+useEffect(() => {
+  fetchAccounts();
+},[]);
+
+  const handleInputChange = (event) => {
+    const value = event.target.type === 'checkbox'? event.target.checked: event.target.value;
+    setFormData({
+      ...formData,
+      [event.target.name]: value,
+    });
+  };
+
+  const handleFormatSumbit = async (event) => {
+    event.preventDefault();
+    await api.post('/accounts/',formData);
+    fetchAccounts();
+    setFormData({
+      user_name: '',
+      password: ''
+    });
+  };
+
+
+  return(
+    <div>
+      <Routes>
+        <Route path="/" exact element={<HomePage/>} />
+        <Route path="/trainee" element={<Trainee/>} />
+        <Route path="/admin" element={<AdminPage/>} />
+      </Routes>
+     
+     
+      {/* <Trainee /> */}
     </div>
-  );
+  )
 }
 
 export default App;
